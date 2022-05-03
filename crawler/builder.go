95ae1f32d62280aaaa00cd1c3bad237caf1f1aa5
Alexandre Lopes <alexandre.lopes@enjoei.com.br>
2022-05-02 21:20:17 -0300

    feat: create movie builder

---
+++ crawler/builder.go
@@ -0,0 +1,58 @@
+package crawler
+
+import (
+	"go-crawler-movie/config"
+	"go-crawler-movie/domain/entity"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+func Build(h *colly.HTMLElement) entity.Movie {
+	movie := entity.Movie{
+		Title:      "",
+		Year:       "",
+		Rating:     0.0,
+		Synopsis:   "",
+		Categories: []string{},
+		Stars:      make([]string, 0),
+		Director:   "",
+	}
+
+	moviePath := getElementPath(h)
+
+	movie.Title = h.ChildText(moviePath.Title)
+	movie.Year = h.ChildText(moviePath.Year)
+	movie.Synopsis = h.ChildText(moviePath.Synopsis)
+	movie.Categories = strings.Split(h.ChildText(moviePath.Categories), ", ")
+	movie.Director = h.ChildText(moviePath.Director)
+	h.ForEach(moviePath.Stars, func(i int, h *colly.HTMLElement) {
+		movie.Stars = append(movie.Stars, h.Text)
+	})
+
+	rating := h.ChildText(moviePath.Rating)
+
+	if rating != "" {
+		converted_rating, err := strconv.ParseFloat(rating, 32)
+
+		if err != nil {
+			movie.Rating = converted_rating
+		}
+	}
+
+	return movie
+}
+
+func getElementPath(h *colly.HTMLElement) config.MovieInfoPath {
+	hasRatingSection := func() bool {
+		rating := h.ChildText(config.WithRatingPath.Rating)
+		return rating != ""
+	}
+
+	if hasRatingSection() {
+		return config.WithRatingPath
+	}
+
+	return config.WithoutRating
+}
