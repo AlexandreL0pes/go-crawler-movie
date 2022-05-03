f19031de8e857346b120848c7a9cbe25ed790c3f
Alexandre Lopes <alexandre.lopes@enjoei.com.br>
2022-05-02 21:23:11 -0300

    feat: create navigate and execute crawler

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -2,7 +2,7 @@ package crawler
 
 import (
 	"fmt"
-	"go-crawler-movie/domain/entity"
+	"go-crawler-movie/domain/repository"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -10,29 +10,33 @@ import (
 )
 
 const MAX_DEPTH = 2
-const ASYNC_ENABLED = true
+const ASYNC_ENABLED = false
 
 const totalIterations = 5
 
 var currentInteration = 0
 
+var url string = "https://www.imdb.com/search/title/?title_type=feature,tv_movie&count=250"
+
 type Crawler struct {
-	collector *colly.Collector
-	Movies    []entity.Movie
+	Collector  *colly.Collector
+	Repository *repository.MoviesRepository
 }
 
 var crawler Crawler
 
-func Initialize() *colly.Collector {
-	crawler.collector = colly.NewCollector(
+func Initialize(r *repository.MoviesRepository) Crawler {
+	crawler.Collector = colly.NewCollector(
 		colly.MaxDepth(MAX_DEPTH),
 		colly.Async(ASYNC_ENABLED),
 	)
 
-	extensions.RandomUserAgent(crawler.collector)
-	extensions.Referer(crawler.collector)
+	crawler.Repository = r
 
-	crawler.collector.Limit(&colly.LimitRule{
+	extensions.RandomUserAgent(crawler.Collector)
+	extensions.Referer(crawler.Collector)
+
+	crawler.Collector.Limit(&colly.LimitRule{
 		DomainGlob:  "*",
 		Parallelism: 4,
 		RandomDelay: 2 * time.Second,
@@ -41,12 +45,14 @@ func Initialize() *colly.Collector {
 	crawler.logOnRequest()
 	crawler.setLanguageHeader()
 	crawler.handleError()
+	crawler.getMovies()
+	crawler.navigate()
 
-	return crawler.collector
+	return crawler
 }
 
 func (c Crawler) logOnRequest() {
-	c.collector.OnRequest(func(r *colly.Request) {
+	c.Collector.OnRequest(func(r *colly.Request) {
 		fmt.Printf("\n\n")
 		fmt.Printf("Crawling a new page...")
 		fmt.Println(r.URL)
@@ -57,24 +63,36 @@ func (c Crawler) logOnRequest() {
 }
 
 func (c Crawler) setLanguageHeader() {
-	c.collector.OnRequest(func(r *colly.Request) {
+	c.Collector.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("Accept-Language", "en-US")
 	})
 }
 
 func (c Crawler) handleError() {
-	c.collector.OnError(func(r *colly.Response, err error) {
+	c.Collector.OnError(func(r *colly.Response, err error) {
 		fmt.Println("\nRequest URL:", r.Request.URL, "failed with response:", r.StatusCode, "\nError:", err)
 		fmt.Println("body: ", r.Body)
 	})
 }
 
+func (c Crawler) getMovies() {
+	c.Collector.OnHTML(".lister-item", func(h *colly.HTMLElement) {
+		movie := Build(h)
+		c.Repository.Save(movie)
+	})
+}
+
+func (c Crawler) Execute() {
+	c.Collector.Visit(url)
+	c.Collector.Wait()
+}
+
 func (c Crawler) navigate() {
-	c.collector.OnHTML("a.lister-page-next", func(e *colly.HTMLElement) {
+	c.Collector.OnHTML("a.lister-page-next", func(e *colly.HTMLElement) {
 		if currentInteration <= totalIterations {
 			nextPage := e.Request.AbsoluteURL(e.Attr("href"))
 			currentInteration++
-			c.collector.Visit(nextPage)
+			c.Collector.Visit(nextPage)
 		}
 	})
 }
