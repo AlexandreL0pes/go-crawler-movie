6178c4e2ddc14e1fc1a47e8587227e9eaf6b007c
Alexandre Lopes <alexandre.lopes@enjoei.com.br>
2022-05-02 21:27:43 -0300

    refact: moves repository and builder methods

--- main.go
+++ main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"go-crawler-movie/config"
 	"go-crawler-movie/crawler"
 	"go-crawler-movie/domain/entity"
+	"go-crawler-movie/domain/repository"
 	"io/ioutil"
-	"strconv"
-	"strings"
 	"time"
-
-	"github.com/gocolly/colly"
 )
-
-// var url string = "https://www.imdb.com/title/tt1877830/?ref_=watch_fanfav_tt_t_1"
 
 // var url string = "https://www.imdb.com/search/title/?title_type=feature,tv_movie&count=250"
 // 620 interations - 155000 movies
@@ -22,69 +16,18 @@ import (
 // 27000
 // 2500
 
-var url string = "https://www.imdb.com/search/title/?title_type=feature,tv_movie&count=250&after=WzE0NzQ2MCwidHQwMDk5NTQ0Iiw5MTAwMV0%3D&ref_=adv_nxt"
-
-const totalIterations = 2
-
-var currentInteration = 0
+// var url string = "https://www.imdb.com/search/title/?title_type=feature,tv_movie&count=250&after=WzE0NzQ2MCwidHQwMDk5NTQ0Iiw5MTAwMV0%3D&ref_=adv_nxt"
 
 func main() {
 	fmt.Println("> executing crawler")
-	scrape(url)
-}
-
-func scrape(url string) {
 	start := time.Now()
+	repository := repository.Initialize()
 
-	movies := []entity.Movie{}
+	c := crawler.Initialize(&repository)
+	c.Execute()
 
-	c := crawler.Initialize()
-
-	c.OnHTML(".lister-item", func(h *colly.HTMLElement) {
-		title := h.ChildText(config.Paths.Title)
-		year := h.ChildText(config.Paths.Year)
-		rating_s := h.ChildText(config.Paths.Rating)
-
-		rating := 0.0
-		synopsis := ""
-		categories := []string{}
-		director := ""
-		stars := make([]string, 0)
-		if rating_s != "" {
-			a, err := strconv.ParseFloat(rating_s, 32)
-			rating = a
-			if err != nil {
-				rating = 0.0
-			}
-
-			synopsis = h.ChildText(config.Paths.Synopsis)
-			categories = strings.Split(h.ChildText(config.Paths.Categories), ", ")
-			director = h.ChildText(config.Paths.Director)
-
-			h.ForEach(config.Paths.Stars, func(i int, h *colly.HTMLElement) {
-				stars = append(stars, h.Text)
-			})
-		} else {
-			rating = 0.0
-
-			synopsis = h.ChildText("div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(3)")
-			categories = strings.Split(h.ChildText(config.Paths.Categories), ", ")
-			director = h.ChildText("div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(4) > a:nth-child(1)")
-
-			h.ForEach("div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(4) > span ~ a", func(i int, h *colly.HTMLElement) {
-				stars = append(stars, h.Text)
-			})
-
-		}
-
-		movies = append(movies, entity.Movie{title, year, rating, synopsis, categories, stars, director})
-
-	})
-
-	c.Visit(url)
-	c.Wait()
+	storeMovies(repository.Movies)
 	fmt.Printf("\n\nTook around %s \n", elapsedTime(start))
-	storeMovies(movies)
 }
 
 func storeMovies(m []entity.Movie) {
