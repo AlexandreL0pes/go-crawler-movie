661dd3c99c48b88703ecf680f8269060afd14e9a
Alexandre Lopes <alexandre.lopes@enjoei.com.br>
2022-05-02 21:21:13 -0300

    feat: move movie dom paths to config file

--- config/config.go
+++ config/config.go
@@ -1,6 +1,6 @@
 package config
 
-type movieInfo struct {
+type MovieInfoPath struct {
 	Title      string
 	Year       string
 	Rating     string
@@ -10,7 +10,7 @@ type movieInfo struct {
 	Director   string
 }
 
-var Paths = movieInfo{
+var WithRatingPath = MovieInfoPath{
 	Title:      "div.lister.list.detail.sub-list > div > div > div.lister-item-content > h3 > a",
 	Year:       "div.lister.list.detail.sub-list > div > div > div.lister-item-content > h3 > span.lister-item-year.text-muted.unbold",
 	Rating:     "div.lister.list.detail.sub-list > div > div > div.lister-item-content > div > div.inline-block.ratings-imdb-rating > strong",
@@ -18,4 +18,14 @@ var Paths = movieInfo{
 	Categories: "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(2) > span.genre",
 	Stars:      "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(5) > span ~ a",
 	Director:   "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(5) > a:nth-child(1)",
+}
+
+var WithoutRating = MovieInfoPath{
+	Title:      "div.lister.list.detail.sub-list > div > div > div.lister-item-content > h3 > a",
+	Year:       "div.lister.list.detail.sub-list > div > div > div.lister-item-content > h3 > span.lister-item-year.text-muted.unbold",
+	Rating:     "div.lister.list.detail.sub-list > div > div > div.lister-item-content > div > div.inline-block.ratings-imdb-rating > strong",
+	Synopsis:   "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(3)",
+	Categories: "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(2) > span.genre",
+	Stars:      "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(4) > span ~ a",
+	Director:   "div.lister.list.detail.sub-list > div > div > div.lister-item-content > p:nth-child(4) > a:nth-child(1)",
 }
